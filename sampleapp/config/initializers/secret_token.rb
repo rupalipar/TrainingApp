1060d9204c017dab3e6ed275762bcdfc71aa6b6c
rupalipar <rupalipar@cybage.com>
2014-08-25 14:25:37 +0530

    Added new repository app

---
+++ sampleapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '4b9ad05c98c5d1fb54a7e97e2ca9115caec50dd3eeba3bbcd18f6b8a270e90d4cbf5940620cc0d5969840f0e2c7bc1f3eb67f96c84a4bc86844a0365ef8e2d35'
